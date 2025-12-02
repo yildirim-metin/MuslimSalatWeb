234e83ded510f83eb4e030f0c7aa8a5fec36ce08
yildirim-metin <metin_yildirim@outlook.be>
2025-12-02 11:07:11 +0100

    activate bootstrapt + create navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkWithHref],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('MuslimSalatWeb');
---
+++ src/app/features/player-time/player-time.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-time',
+  imports: [],
+  templateUrl: './player-time.html',
+  styleUrl: './player-time.scss',
+})
+export class PlayerTime {
+
+}
