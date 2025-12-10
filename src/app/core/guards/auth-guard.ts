5b4e28bfc306917a4c73f1b299af87383c41b9ee
yildirim-metin <metin_yildirim@outlook.be>
2025-12-10 15:49:24 +0100

    check admin in auth guard

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,14 +1,14 @@
 import { inject } from '@angular/core';
 import { Router, CanActivateFn } from '@angular/router';
-import { AuthService } from '../services/auth.service'; 
+import { AuthService } from '../services/auth.service';
+import { UserRole } from '@core/enums/user-role.enum';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  
-  if (authService.isConnected()) { 
-    return true; 
+  if (authService.role() === UserRole.Admin) {
+    return true;
   } else {
     router.navigate(['/login']);
     return false;
